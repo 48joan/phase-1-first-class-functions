64582b7ec7cd3436532874729c50111e86277042
joan <joan.cherono@student.moringaschool.com>
2024-03-24 15:52:07 +0300

    Done

---
+++ index.js
@@ -0,0 +1,22 @@
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(num1){
+    return function (fare) {
+        return num1 * fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier (2);
+
+const fareTripler = createFareMultiplier (3);
+
+const selectDifferentDrivers= function(driversArray, driversReturned){
+    return driversReturned(driversArray);
